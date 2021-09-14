ea9fe899a1981a4e1ef812c97842e190f84bf77f
Corey Beebe <cbb@hey.com>
2021-09-14 15:31:42 -0400

    Updated String extension to Swift 5.

--- Sources/CBsBoT/Retroactive Modeling/Standard Library/String.swift
+++ Sources/CBsBoT/Retroactive Modeling/Standard Library/String.swift
@@ -38,7 +38,7 @@ public extension String {
     
     func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
          let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
-         let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
 
          return ceil(boundingBox.width)
      }
