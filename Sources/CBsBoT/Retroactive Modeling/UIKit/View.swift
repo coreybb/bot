685fea32f61e664a2907de2424fc94938111e345
Corey Beebe <cbb@hey.com>
2021-10-06 16:00:18 -0400

    Made the new View class public and open.

--- Sources/CBsBoT/Retroactive Modeling/UIKit/View.swift
+++ Sources/CBsBoT/Retroactive Modeling/UIKit/View.swift
@@ -9,11 +9,15 @@ import UIKit
 
 
 /// A custom class intended to be used in lieu of `UIView` across projects relying exclusively on UIKit and Autolayout. Its only function is to universally set `translatesAutoresizingMaskIntoConstraints` to false.
-class View: UIView  {
+open class View: UIView  {
     
-    override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
         
         translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
