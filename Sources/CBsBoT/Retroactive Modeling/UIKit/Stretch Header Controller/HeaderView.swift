f515abfdbe707da88979c4e797ec271119a4c8c8
Corey Beebe <cbb@hey.com>
2021-10-09 16:41:47 -0400

    Testing changes to at which point in the application lifecycle the UI is laid out for the StretchHeaderController.

--- Sources/CBsBoT/Retroactive Modeling/UIKit/Stretch Header Controller/HeaderView.swift
+++ Sources/CBsBoT/Retroactive Modeling/UIKit/Stretch Header Controller/HeaderView.swift
@@ -15,7 +15,10 @@ internal class HeaderView: UICollectionReusableView {
     //----------------------------
     //  MARK: - Public Properties
     //----------------------------
-    var childView: UIView?
+    var childView: UIView? {
+        addSubview(childView!)
+        childView?.fillSuperview()
+    }
 
     
     
@@ -26,22 +29,9 @@ internal class HeaderView: UICollectionReusableView {
         super.init(frame: .zero)
         
         backgroundColor = .clear
-        addSubview(childView!)
-        childView?.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    
-
-    //-------------------------------
-    //  MARK: - Superclass Overrides
-    //-------------------------------
-    open override func layoutSubviews() {
-        super.layoutSubviews()
-
-
     }
 }
