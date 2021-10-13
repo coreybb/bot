31950cf12b392b3b100fa56d74cf7ee4c7471977
Corey Beebe <cbb@hey.com>
2021-10-13 18:47:06 -0400

    Added a class RoundedImageView, abstracting away the need to embed a UIImageView inside of a UIView in order to cast as drop shadow, while also handling configuration of the layer and its cornerRadii for free.

---
+++ Sources/CBsBoT/Retroactive Modeling/UIKit/RoundedImageView.swift
@@ -0,0 +1,94 @@
+//
+//  RoundedImageView.swift
+//  
+//
+//  Created by Corey Beebe on 10/13/21.
+//
+
+import UIKit
+
+
+open class RoundedImageView: View {
+    
+    
+    //------------------------
+    //  MARK: - Corner Radius
+    //------------------------
+    private let circular: Bool
+    private let cornerRadius: CGFloat
+    private let image: UIImage
+    private let castsShadow: Bool
+    private var didLayoutSubviews: Bool = false
+    
+    
+    
+    //---------------
+    //  MARK: - Init
+    //---------------
+    public init(image: UIImage, cornerRadius: CGFloat = 11, circular: Bool = false, castsShadow: Bool = true) {
+        self.image = image
+        self.circular = circular
+        self.cornerRadius = cornerRadius
+        self.castsShadow = castsShadow
+        super.init(frame: .zero)
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+    
+    //-------------------------------
+    //  MARK: - Superclass Overrides
+    //-------------------------------
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if didLayoutSubviews { return }
+        setupUI()
+        didLayoutSubviews = true
+    }
+    
+    
+    
+    //----------------------
+    //  MARK: - Private API
+    //----------------------
+    private func setupUI() {
+        
+        clipsToBounds = false
+        
+        if castsShadow {
+            addShadow()
+        }
+        
+        layoutImage()
+    }
+    
+    
+    private func addShadow() {
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 10
+        layer.drawsAsynchronously = true
+        layer.shouldRasterize = true
+        layer.rasterizationScale = UIScreen.main.scale
+        layer.shadowOffset = .init(width: 1, height: 2)
+        
+        let shadowCornerRadius = circular ? (frame.height / 2) : self.cornerRadius
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: shadowCornerRadius).cgPath
+    }
+    
+    
+    private func layoutImage() {
+        
+        let imageView: UIImageView = UIImageView(image: image)
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = circular ? (frame.height / 2) : cornerRadius
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        imageView.fillSuperview()
+    }
+}
