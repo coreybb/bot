950f7494c196c3bd95540df7bccabc7db0b0e16f
Corey Beebe <81270300+lfsvr-admin@users.noreply.github.com>
2021-10-24 14:26:08 -0400

    Cleaned up the serialization error message for HTTPError.

--- Sources/CBsBoT/HTTP/HTTPError.swift
+++ Sources/CBsBoT/HTTP/HTTPError.swift
@@ -42,7 +42,7 @@ public extension HTTPError {
         case .error(let message): return message.localizedDescription
         case .unknown(let message): return message
         case .serialization(codingError: let codingError):
-            return "\nWe were unable to decode the data returned in an HTTP response. Here's the serialization error:\n\(codingError.serializationError)\nPlease inspect the data returned to ensure that it matches what your data model(s):\n\(codingError.dataString)\n"
+            return "\nWe were unable to decode data returned in an HTTP response. The serialization error should specify the mislabelled or missing key:\n\(codingError.serializationError)\nPlease inspect the response data below to ensure that it matches your object model(s):\n\(codingError.dataString)\n"
         }
     }
 }
