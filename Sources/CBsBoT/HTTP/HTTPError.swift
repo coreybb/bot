b31ce139bb43453be1edbdf372bbea052345b0ac
Corey Beebe <81270300+lfsvr-admin@users.noreply.github.com>
2021-10-24 14:10:18 -0400

    Added detailed descriptions for HTTP errors so that the developer does not have to switch through cases in order to print an error message.

--- Sources/CBsBoT/HTTP/HTTPError.swift
+++ Sources/CBsBoT/HTTP/HTTPError.swift
@@ -23,3 +23,26 @@ public enum HTTPError {
     case unknown(String)
     case serialization(codingError: CodingError)
 }
+
+
+public extension HTTPError {
+    
+    var message: String {
+        
+        switch self {
+        case .ok: return "The server returned an \"OK\" status."
+        case .moved: return "Service at the specified endpoint has moved."
+        case .badRequest: return "You've sent a bad request to the server."
+        case .unauthorized: return "You're not authorized to access this service."
+        case .forbidden: return "Access is forbidden at this endpoint."
+        case .notFound: return "There was no service found at this endpoint."
+        case .internalServerError: return "There was an internal server error at the specified endpoint. Try again later."
+        case .unavailable: return "The service at this endpoint is currently unavailable."
+        case .noResponse: return "There was no response from the specified endpoint."
+        case .error(let message): return message.localizedDescription
+        case .unknown(let message): return message
+        case .serialization(codingError: let codingError):
+            return "\nWe were unable to decode the data returned in an HTTP response. Here's the serialization error:\n\(codingError.serializationError)\nPlease inspect the data returned to ensure that it matches what your data model(s):\n\(codingError.dataString)\n"
+        }
+    }
+}
