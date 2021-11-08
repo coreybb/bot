e30b786c1f568cff6750605f3f33df5857291bcc
Corey Beebe <81270300+lfsvr-admin@users.noreply.github.com>
2021-11-08 15:27:54 -0500

    Finished the reusable CircularButton class. Might need some further testing using just text and background colors.

--- Sources/CBsBoT/Retroactive Modeling/UIKit/RoundedButton.swift
+++ Sources/CBsBoT/Retroactive Modeling/UIKit/RoundedButton.swift
@@ -1,47 +1,73 @@
 //
-//  RoundedButton.swift
-//  
+//  CircularButton.swift
+//  Lifesaver
 //
-//  Created by Corey Beebe on 11/7/21.
+//  Created by Corey Beebe on 11/8/21.
 //
 
 import UIKit
 
 
-open class RoundedButton: CustomButton {
+class CircularButton: CustomButton {
     
     
     //-----------------------------
     //  MARK: - Private Properties
     //-----------------------------
-    private var color: UIColor
-    private var textColor: UIColor?
-    private var text: String?
-    private var font: UIFont?
-    private var cornerRadius: CGFloat
-    private var isCircular: Bool
-    private var image: UIImage?
     private var didLayoutSubviews: Bool = false
-    
-    
-    //----------------------
-    //  MARK: - Public Init
-    //----------------------
-    public init(color: UIColor = .cardWhite, textColor: UIColor? = nil, text: String? = nil, font: UIFont? = nil, image: UIImage? = nil, cornerRadius: CGFloat = 15, isCircular: Bool = false) {
+    private let color: UIColor?
+    private let text: String?
+    private let textColor: UIColor?
+    private let font: UIFont?
+    private let image: UIImage?
+    private let cornerRadius: CGFloat
+    private let isCircular: Bool
+    private let castsShadow: Bool
+
+    private lazy var shadowLayer: CALayer = {
+       
+        let layer: CALayer = CALayer()
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 10
+        layer.drawsAsynchronously = true
+        layer.shouldRasterize = true
+        layer.rasterizationScale = UIScreen.main.scale
+        layer.shadowOffset = .init(width: 1, height: 2)
+        layer.masksToBounds = false
+        return layer
+    }()
+
+    private lazy var imageLayer: CALayer? = {
         
+        guard let image = image else { return nil }
+        let layer: CALayer = CALayer()
+        layer.frame = bounds
+        layer.contents = image.cgImage
+        layer.masksToBounds = true
+        return layer
+    }()
+    
+    
+    
+    //---------------
+    //  MARK: - Init
+    //---------------
+    public init(color: UIColor? = nil, text: String? = nil, textColor: UIColor? = nil, font: UIFont? = nil, image: UIImage? = nil, cornerRadius: CGFloat = 15, isCircular: Bool = false, castsShadow: Bool = false) {
         self.color = color
-        self.textColor = textColor
         self.text = text
+        self.textColor = textColor
         self.font = font
+        self.image = image
         self.cornerRadius = cornerRadius
         self.isCircular = isCircular
-        self.image = image
+        self.castsShadow = castsShadow
         super.init(frame: .zero)
-        translatesAutoresizingMaskIntoConstraints = false
+        performInitialSetup()
     }
     
     required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError()
     }
     
     
@@ -49,12 +75,9 @@ open class RoundedButton: CustomButton {
     //-------------------------------
     //  MARK: - Superclass Overrides
     //-------------------------------
-    override open func layoutSubviews() {
-        super.layoutSubviews()
-        
-        if didLayoutSubviews { return }
-        setup()
-        didLayoutSubviews.toggle()
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        setupLayers()
     }
     
     
@@ -62,22 +85,31 @@ open class RoundedButton: CustomButton {
     //----------------------
     //  MARK: - Private API
     //----------------------
-    private func setup() {
+    private func performInitialSetup() {
         
-        setupLayer()
+        translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = color
-        titleLabel?.font = font
+        setTitle(text, for: .normal)
         setTitleColor(textColor, for: .normal)
-        setImage(image, for: .normal)
+        titleLabel?.font = font
     }
     
     
-    private func setupLayer() {
+    private func setupLayers() {
 
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 2, height: 2)
-        layer.shadowOpacity = 0.066
-        layer.shadowRadius = 1
         layer.cornerRadius = isCircular ? (frame.height / 2) : cornerRadius
+        
+        if let imageLayer = imageLayer {
+            layer.insertSublayer(imageLayer, at: 0)
+            imageLayer.cornerRadius = layer.cornerRadius
+            imageLayer.frame = bounds
+        }
+        
+        if castsShadow {
+            layer.insertSublayer(shadowLayer, at: 0)
+            shadowLayer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: layer.cornerRadius).cgPath
+            shadowLayer.cornerRadius = layer.cornerRadius
+        }
     }
 }
+
