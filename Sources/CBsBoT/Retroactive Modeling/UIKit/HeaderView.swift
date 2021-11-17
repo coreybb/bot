981d80900a573d288e1f294a43e91947b638d3e5
Corey Beebe <81270300+lfsvr-admin@users.noreply.github.com>
2021-11-17 15:03:56 -0500

    Replaced the reusable  class for s with an open implementation.

---
+++ Sources/CBsBoT/Retroactive Modeling/UIKit/HeaderView.swift
@@ -0,0 +1,45 @@
+//
+//  HeaderView.swift
+//  
+//
+//  Created by Corey Beebe on 11/17/21.
+//
+
+import UIKit
+
+
+///  A simple subclass of `UICollectionReusableView` that exposes a `childView` property and lays it out to fill the containing `HeaderView`.
+open class HeaderView: UICollectionReusableView {
+    
+    
+    //----------------------------
+    //  MARK: - Public Properties
+    //----------------------------
+    var childView: UIView?
+    
+    
+    
+    //---------------
+    //  MARK: - Init
+    //---------------
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    
+    
+    //-------------------------------
+    //  MARK: - Superclass Overrides
+    //-------------------------------
+    open override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        backgroundColor = .clear
+        addSubview(childView!)
+        childView?.fillSuperview()
+    }
+}
